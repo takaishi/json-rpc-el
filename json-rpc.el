95d4b28c9744074002acd07521910aaf5a99aa64
Ryo TAKAISHI <takaishi_r@ariel-networks.com>
2012-08-30 15:29:18 +0900

    plistで書いたクエリのパラメタを変換できるようになった

--- json-rpc.el
+++ json-rpc.el
@@ -1,1 +1,18 @@
+(require 'json)
+
+(defun json-rpc:join-plist (glue plist)
+  (let (result)
+  (while plist
+	(push (concat (substring (symbol-name (car plist)) 1) glue (cadr plist))
+		  result)
+	(setq plist (cddr plist)))
+  result))
+
+(defun json-rpc:make-data (plist)
+  (if (member :params plist)
+	  (if  (plist-get plist :params)
+		  (setq plist (plist-put plist :params (vconcat (json-rpc:join-plist "=" (plist-get plist :params)))))
+		(setq plist (plist-put plist :params []))))
+  (json-encode-plist plist))
+
 (provide 'json-rpc)
--- test-json-rpc.el
+++ test-json-rpc.el
@@ -1,7 +1,20 @@
 (require 'ert)
 (require 'json-rpc)
 
+(ert-deftest json-rpc:join-plist ()
+  (should (equal '("owner=takaishi_r")
+				 (json-rpc:join-plist "=" '(:owner "takaishi_r"))))
+  )
+
 (ert-deftest json-rpc ()
-  (should (equal "1" "1")))
+  (should (equal "{\"jsonrpc\":\"1.0\"}"
+				 (json-rpc:make-data '(:jsonrpc "1.0"))))
+  (should (equal "{\"jsonrpc\":\"1.0\", \"method\":\"getinfo\"}"
+				 (json-rpc:make-data '(:jsonrpc "1.0" :method "getinfo"))))
+  (should (equal "{\"params\":[]}"
+				 (json-rpc:make-data '(:params nil))))
+  (should (equal "{\"params\":[\"owner=takaishi_r\"]}"
+				 (json-rpc:make-data '(:params (:owner "takaishi_r")))))
+  )
 
 (ert-run-tests-batch-and-exit)
