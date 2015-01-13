005b159f60e9db4c8f360729fd61e10b7f15450b
takaishi_r <takaishi_r@ariel-networks.com>
2015-01-13 17:01:37 +0900

    add call-method

--- test-json-rpc.el
+++ test-json-rpc.el
@@ -15,6 +15,12 @@
 				 (json-rpc:make-data '(:params nil))))
   (should (equal "{\"params\":[\"owner=takaishi_r\"]}"
 				 (json-rpc:make-data '(:params (:owner "takaishi_r")))))
+  (should (equal "{\"params\":[\"1000\"]}"
+				 (json-rpc:make-data '(:params ("1000")))))
+  ;; (should (equal "{\"params\":[\"1000\"]}"
+  ;; 				 (json-rpc:make-data '(:params (1000)))))
+  (should (equal "{\"params\":[\"owner=takaishi_r&changetime=..\"]}"
+				 (json-rpc:make-data '(:params (:owner "takaishi_r" :changetime "..")))))
   )
 
 (ert-run-tests-batch-and-exit)
